6ecbca9f7a48207a3b5f0ebc7673ed4bc20aa0c8
Mama Mama <sirdiogo@seznam.cz>
2021-11-19 17:12:11 +0100

    burger scroll fix

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,5 +1,19 @@
 let menu_open = false;
 var navbar = document.getElementById('nav');
+var links = document.querySelectorAll('.link-text');
+
+/* fix for scroll */
+for(var link of links){
+    link.addEventListener("click", ()=>{
+        if(menu_open){
+            burger();
+            setTimeout(function() { smoothScroll('.chart', 1000); }, 250);
+        }
+        else{
+            smoothScroll('.chart', 1000);
+        }
+    })
+}
 
 const burger_menu = document.getElementById('burger').addEventListener("click", ()=>{
     burger();
@@ -23,3 +37,4 @@ function burger(){
         menu_open = false;
     }
 }
+
